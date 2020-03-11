07d30f71e654283a62e5bd295356ed303bc0109f
GuillemSune97 <gsunemontana@gmail.com>
2020-03-11 13:38:05 +0100

    Admin panel & new models

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,19 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    birth_date = models.DateTimeField()
 
     def __str__(self):
-        return self.title
+        return self.name
+
+class Car(models.Model):
+    color = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
+    purchase_date = models.DateTimeField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.color + ' ' + self.brand
